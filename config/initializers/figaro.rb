0e1481e6aa8639c1f0f2145cb44b5929c3e5d227
kmrd <david@thoughtbubble.ca>
2015-03-07 11:46:45 -0500

    aws get it working

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Figaro.require_keys("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_BUCKET_NAME", "FOG_DIRECTORY",
+  Figaro.require_keys("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_BUCKET_NAME", "FOG_DIRECTORY"
                     #"EMAIL_USER", "EMAIL_PASS", "EMAIL_HOST", "EMAIL_PORT",
                     #"STRIPE_SECRET_KEY", "STRIPE_PUBLISHABLE_KEY"
                     )
