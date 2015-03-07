273331838ec805af37336a057e103da4f19a5606
kmrd <david@thoughtbubble.ca>
2015-03-07 15:59:30 -0500

    New seed with tenative test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-u = User.create(name: 'Kirkpatrick Macmillan', email: 'joyride@gmail.com')
-d = Device.create(name: 'Penny-farthing', user: u)
+Coord.create!([
+  {lat: "43.659891", lng: "-79.358625", device_id: 1, state: "journey"},
+  {lat: "43.657891", lng: "-79.368625", device_id: 1, state: "journey"},
+  {lat: "43.654891", lng: "-79.388625", device_id: 1, state: "journey"},
+  {lat: "43.654891", lng: "-79.388625", device_id: 1, state: "alert"},
+  {lat: "43.653891", lng: "-79.386625", device_id: 1, state: "alert"}
+])
+Device.create!([
+  {name: "Penny-farthing", user_id: 1}
+])
+User.create!([
+  {name: "Kirkpatrick Macmillan", email: "joyride@gmail.com", meta: {"text_notification"=>"4168418601", "email_notification"=>"joyride@gmail.com"}}
+])
